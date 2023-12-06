08f77d76be87ffd5678e73aab276a5d7a2409f32
c4nkn <cankahraman05@gmail.com>
2023-12-06 20:21:45 +0300

    Created GUI components and events for Menu.

--- src/Menu.java
+++ src/Menu.java
@@ -1,2 +1,40 @@
-public class Menu {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Menu extends JFrame {
+    private JPanel MainPanel;
+    private JPanel ActionPanel;
+    private JLabel image;
+    private JButton addStudentButton;
+    private JButton addCourseButton;
+
+    public Menu() {
+        setTitle("School Dashboard");
+        setSize(400, 400);
+        setResizable(false);
+        setLocation(200,200);
+        setContentPane(MainPanel);
+
+        addStudentButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        addCourseButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new Menu();
+    }
 }
